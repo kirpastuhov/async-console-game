bc2e0e32c4bd36be96c666fba769839df4d8c8cc
Kirill Pastukhov <kirush-ka@yandex.ru>
2022-04-14 16:10:38 +0300

    Update game.py

--- game.py
+++ game.py
@@ -11,42 +11,41 @@ STAR_SYMBOLS = ["+", "*", ".", ":"]
 
 async def blink(canvas, row, column, symbol="*"):
     while True:
-        canvas.border()
         curses.curs_set(False)
-        canvas.addstr(row, column, symbol, curses.A_DIM)
-        i = random.randint(3, 20)
+        star_state_time = random.randint(3, 20)
 
-        for _ in range(i):
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        for _ in range(star_state_time):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(i):
+        for _ in range(star_state_time):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(i):
+        for _ in range(star_state_time):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(i):
+        for _ in range(star_state_time):
             await asyncio.sleep(0)
 
 
-def get_coordinates(max_y, max_x):
-    offset = 3
-    new_y = random.randint(0, max_y - offset)
-    new_x = random.randint(0, max_x - offset)
+def get_star_coordinates(max_y, max_x):
+    offset = 1
+    new_y = random.randint(1, max_y - offset)
+    new_x = random.randint(1, max_x - offset)
 
     return new_y, new_x
 
 
 def generate_stars(canvas):
-    rows, columns = canvas.getmaxyx()
-    max_row, max_column = rows - 1, columns - 1
+    max_y, max_x = canvas.getmaxyx()
+    max_row, max_column = max_y - 1, max_x - 1
     coroutines = []
     for _ in range(250):
         symbol = random.choice(STAR_SYMBOLS)
-        row, column = get_coordinates(max_row, max_column)
+        row, column = get_star_coordinates(max_row, max_column)
         star = blink(canvas, row, column, symbol)
         coroutines.append(star)
 
@@ -55,19 +54,22 @@ def generate_stars(canvas):
 
 def draw(canvas):
     canvas.nodelay(True)
-    rows, columns = canvas.getmaxyx()
+    max_y, max_x = canvas.getmaxyx()
 
     frames = []
     with open("animations/frames/rocket_frame_1.txt", "r") as f:
-        frames.append(f.read())
+        frame = f.read()
+        frames.extend([frame, frame])
     with open("animations/frames/rocket_frame_2.txt", "r") as f:
-        frames.append(f.read())
+        frame = f.read()
+        frames.extend([frame, frame])
 
-    spaceship_animation = animate_spaceship(canvas, rows / 2, columns / 2, frames)
+    spaceship_animation = animate_spaceship(canvas, max_y / 2, max_x / 2, frames)
     coroutines = [spaceship_animation]
     coroutines.extend(generate_stars(canvas))
 
     while True:
+        canvas.border()
         for coroutine in coroutines.copy():
             try:
                 coroutine.send(None)
